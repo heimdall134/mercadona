2400e1b47716c83916dcc23708157306371629ca
fernandogutierrez <fernando.gutierrez@cobee.io>
2024-07-26 09:25:28 +0200

    created EAN test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,113 @@
-import { main } from "./index";
+import { Extractor } from "./index";
 
 describe("index", () => {
-  it("should log hello world", async () => {
-    const logSpy = jest.spyOn(console, "log").mockImplementation();
+  describe("starting 84 EAN digit", () => {
+    const digitCodes = [{
+      digitCode: "8480000278623",
+      expectedMercaCode: "27862"
+    }, {
+      digitCode: "8480000298623",
+      expectedMercaCode: "29862"
+    }];
 
-    await main();
+    it("extra merca code expected 27862", () => {
+      const extractor = new Extractor(digitCodes[0].digitCode);
+      const mercaCode = extractor.mercaCode();
 
-    expect(logSpy).toHaveBeenCalledWith("hello world!");
+      expect(mercaCode).toBe(digitCodes[0].expectedMercaCode);
+    });
 
-    logSpy.mockRestore();
+    it("extra merca code expected 29862", () => {
+      const extractor = new Extractor(digitCodes[1].digitCode);
+      const mercaCode = extractor.mercaCode();
+
+      expect(mercaCode).toBe(digitCodes[1].expectedMercaCode);
+    });
   });
+
+  describe("starting 23 EAN digit", () => {
+
+    it("extract merca code for 13 digits", () => {
+      const digitCode = "2369664001999";
+      const extractor = new Extractor(digitCode);
+      const mercaCode = extractor.mercaCode();
+
+      expect(mercaCode).toBe("69664");
+
+    });
+
+    it("extract merca code for 13 digits", () => {
+      const digitCode = "2370664001999";
+      const extractor = new Extractor(digitCode);
+      const mercaCode = extractor.mercaCode();
+
+      expect(mercaCode).toBe("70664");
+
+    });
+
+    it("extract price for 13 digits", () => {
+      const digitCode = "2370664001999";
+      const extractor = new Extractor(digitCode);
+      const priceInEuro = extractor.priceInEuro();
+
+      expect(priceInEuro).toBe("00199");
+
+    });
+
+    it("extract price for 13 digits", () => {
+      const digitCode = "2370664001989";
+      const extractor = new Extractor(digitCode);
+      const priceInEuro = extractor.priceInEuro();
+
+      expect(priceInEuro).toBe("00198");
+
+    });
+
+    it("extract merca code for 24 digits", () => {
+      const digitCode = "230036490033000165000542";
+      const extractor = new Extractor(digitCode);
+      const mercaCode = extractor.mercaCode();
+
+      expect(mercaCode).toBe("03649");
+
+    });
+
+    it("extract merca code for 24 digits", () => {
+      const digitCode = "230046490033000165000542";
+      const extractor = new Extractor(digitCode);
+      const mercaCode = extractor.mercaCode();
+
+      expect(mercaCode).toBe("04649");
+
+    });
+
+    it("extract price for 24 digits", () => {
+      const digitCode = "230046490033000165000542";
+      const extractor = new Extractor(digitCode);
+      const priceInEuro = extractor.priceInEuro();
+
+      expect(priceInEuro).toBe("00054");
+
+    });
+
+    it("extract pvp for 24 digits", () => {
+      const digitCode = "230046490033000165000542";
+      const extractor = new Extractor(digitCode);
+      const pvp = extractor.pvp();
+
+      expect(pvp).toBe("00165");
+
+    });
+
+    it("extract pvp for 24 digits", () => {
+      const digitCode = "230046490033000166000542";
+      const extractor = new Extractor(digitCode);
+      const pvp = extractor.pvp();
+
+      expect(pvp).toBe("00166");
+
+    });
+  });
+
 });
+
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,38 @@ dotenv.config();
 
 export const main = async (): Promise<void> => {
   console.log("hello world!");
+
 };
+
+export class Extractor {
+  private codeDigits: string;
+  private initialDigits: string;
+
+  constructor(codeDigits: string) {
+    this.codeDigits = codeDigits;
+    this.initialDigits = this.codeDigits.substring(0, 2);
+  }
+
+  public mercaCode(): string {
+    if (this.initialDigits == "84") {
+      return this.codeDigits.substring(7, 12);
+    } else if (this.initialDigits == "23" && this.codeDigits.length == 13) {
+      return this.codeDigits.substring(2, 7);
+    } else {
+      return this.codeDigits.substring(3, 8);
+    }
+  }
+
+  public priceInEuro(): string {
+    if (this.codeDigits.length == 13) {
+      return this.codeDigits.substring(7, 12);
+    }
+    return this.codeDigits.substring(18, 23);
+  }
+
+  public pvp(): string {
+    return this.codeDigits.substring(13, 18);
+  }
+}
 
 main();
